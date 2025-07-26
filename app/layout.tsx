67b8158b942a90cf9d22e88a77d429fec284016c
azuuurri <itsmeshaun83@gmail.com>
2025-07-26 23:27:34 +0800

    Fix: properly inject title tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
-import './globals.css'
+import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
-import Head from 'next/head'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Chibitsu',
+  description: 'Your new buddies on the blockchain.',
+  generator: 'v0.dev',
+}
 
 export default function RootLayout({
   children,
@@ -9,21 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <Head>
-        <title>Chibitsu</title>
-        <meta name="description" content="Your new buddies on the blockchain." />
-        <meta name="generator" content="v0.dev" />
-      </Head>
-      <body
-        style={{
-          fontFamily: GeistSans.style.fontFamily,
-          ['--font-sans' as any]: GeistSans.variable,
-          ['--font-mono' as any]: GeistMono.variable,
-        }}
-      >
-        {children}
-      </body>
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+      <body>{children}</body>
     </html>
   )
 }
